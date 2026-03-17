95535feb1a8c24e727fccbf1800d16910615bcd6
toyetoye <38019569+toyetoye@users.noreply.github.com>
2026-03-17 11:46:25 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('railway.internal')
+    ? false
+    : process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 const initDB = async () => {
